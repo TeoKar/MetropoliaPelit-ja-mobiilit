097ac3e6582bf9fdd6e38178f81d2f80ad15de86
Max1t <36257728+Max1t@users.noreply.github.com>
2018-04-23 10:32:01 +0300

    Add files via upload

---
+++ Max/Scripts/InventoryScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// Author Max Nikander
+/// Scripting for the inventory system >WIP<
+/// </summary>
+public class InventoryScript : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    private Button daggersButton;
+    private Button healButton;
+
+    private GameObject daggersPanel;
+    private GameObject healPanel;
+
+    private GameObject daggersTrashPanel;
+    private GameObject healTrashPanel;
+
+
+
+
+
+    void Start()
+    {
+        daggersButton = GameObject.Find("DaggersButton").GetComponent<Button>();
+        healButton = GameObject.Find("HealButton").GetComponent<Button>();
+        daggersPanel = GameObject.Find("DaggersPanel");
+        healPanel = GameObject.Find("HealPanel");
+        daggersTrashPanel = GameObject.Find("DaggersTrashPanel");
+        healTrashPanel = GameObject.Find("HealTrashPanel");
+        daggersTrashPanel.SetActive(false);
+        healTrashPanel.SetActive(false);
+        
+        daggersButton.onClick.AddListener(() => ButtonPressed(daggersButton));
+        healButton.onClick.AddListener(() => ButtonPressed(healButton));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ButtonPressed(Button button)
+    {
+        if (button == daggersButton)
+        {
+            Debug.Log("DaggersButtonPressed");
+            daggersPanel.SetActive(false);
+            daggersTrashPanel.SetActive(true);
+
+        }
+        if (button == healButton)
+        {
+            Debug.Log("HealButtonPressed");
+            healPanel.SetActive(false);
+            healTrashPanel.SetActive(true);
+
+        }
+    }
+}
---
+++ Max/Scripts/PauseMenu.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Author Max Nikander
+/// Scripting for the pause menu.
+/// </summary>
+public class PauseMenu : MonoBehaviour
+{
+
+    private Button continueButton;
+    private Button toMainMenuButton;
+
+    private GameObject pauseMenuPanel;
+    private bool pauseOn = false;
+
+    public bool PauseOn
+    {
+        get
+        {
+            return pauseOn;
+        }
+
+        set
+        {
+            pauseOn = value;
+        }
+    }
+
+    // Use this for initialization
+    /// <summary>
+    /// Adds references to game objects and listeners for buttons.
+    /// </summary>
+    void Start()
+    {
+
+        continueButton = GameObject.Find("ContinueButton").GetComponent<Button>();
+        toMainMenuButton = GameObject.Find("ToMainMenuButton").GetComponent<Button>();
+
+        pauseMenuPanel = GameObject.Find("PauseMenuPanel");
+        pauseMenuPanel.SetActive(false);
+
+        continueButton.onClick.AddListener(() => OnContinueButtonClick());
+        toMainMenuButton.onClick.AddListener(() => OnToMainMenuButtonClick());
+
+    }
+
+    // Update is called once per frame
+    /// <summary>
+    /// Gets inputs from escape key and pauses the game when escape is pressed.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown("escape") && PauseOn == false)
+        {
+            Debug.Log("Pause On!");
+            PauseOn = true;
+            pauseMenuPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else if (Input.GetKeyDown("escape") && PauseOn == true)
+        {
+            Debug.Log("Pause Off!");
+            PauseOn = false;
+            pauseMenuPanel.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    /// <summary>
+    /// Continue buttons listener, sets the timescale to 1.0 (default) and closes the pause menu panel.
+    /// </summary>
+    void OnContinueButtonClick()
+    {
+        PauseOn = false;
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    /// <summary>
+    /// Main menu buttons listener, loads the scene that contains the main menu.
+    /// </summary>
+    void OnToMainMenuButtonClick()
+    {
+        SceneManager.LoadScene("Main Manu");
+    }
+    /// <summary>
+    /// Used to acces the state of pauseOn boolean so that other classes know if the game is currently paused.
+    /// </summary>
+    /// <returns>pauseOn boolean</returns>
+    public bool GetpauseOn()
+    {
+        return pauseOn;
+    }
+    }
