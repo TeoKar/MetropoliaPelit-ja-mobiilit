e3259dd679fab4e545b305e9db376acf3c6f8134
Max1t <36257728+Max1t@users.noreply.github.com>
2018-04-23 10:19:04 +0300

    Add files via upload

---
+++ Max/Scripts/CameraScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Adapted from 
+ * https://www.youtube.com/watch?v=Ta7v27yySKs
+*/
+public class CameraScript : MonoBehaviour {
+
+    // Constants to limit camera movement
+    private const float y_Axis_Min = 0.5f;
+    private const float y_Axis_Max = 50.0f;
+
+    public Transform lookingAt;
+    public Transform cameraTransform;
+
+    private Camera MainCamera;
+
+    private float distance = 10.0f;
+    private float currentXaxis = 0.0f;
+    private float currentYaxis = 0.0f;
+
+    [SerializeField] private float sensitivityXaxis = 4.0f;
+    [SerializeField] private float sensitivityYaxis = 1.0f;
+
+    private void Start()
+    {
+
+        // 
+        cameraTransform = transform;
+        MainCamera = Camera.main;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // User mouseinput for camera axis
+        currentXaxis += Input.GetAxis("Mouse X");
+        currentYaxis += Input.GetAxis("Mouse Y");
+        // Limits camera movement between constants
+        currentYaxis = Mathf.Clamp(currentYaxis, y_Axis_Min, y_Axis_Max);
+    }
+
+    private void LateUpdate()
+    {
+        // Camera movement vector and rotation
+        Vector3 direction = new Vector3(0, 0, -distance);   
+        Quaternion rotation = Quaternion.Euler(currentYaxis, currentXaxis, 0);
+        cameraTransform.position = lookingAt.position + rotation * direction;
+        cameraTransform.LookAt(lookingAt.position);
+
+    }
+}
+
+
---
+++ Max/Scripts/MenuScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Author Max Nikander
+/// Scripting for the main menu scene.
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+
+
+    private Button startButton;
+    private Button creditsButton;
+    private Button backButton;
+    private Button exitButton;
+
+    private GameObject mainMenuPanel;
+    private GameObject creditsMenuPanel;
+
+
+    // Use this for initialization
+    /// <summary>
+    /// Add references to game objects and listeners to buttons.
+    /// </summary>
+    void Start()
+    {
+        mainMenuPanel = GameObject.Find("MainMenuPanel");
+        creditsMenuPanel = GameObject.Find("CreditsMenuPanel");
+
+        startButton = GameObject.Find("StartButton").GetComponent<Button>();
+        creditsButton = GameObject.Find("CreditsButton").GetComponent<Button>();
+        backButton = GameObject.Find("BackButton").GetComponent<Button>();
+        exitButton = GameObject.Find("ExitButton").GetComponent<Button>();
+
+        startButton.onClick.AddListener(()=> OnStartButtonClick());
+        creditsButton.onClick.AddListener(() => OnCreditsButtonClick());
+        backButton.onClick.AddListener(() => OnBackButtonClick());
+        exitButton.onClick.AddListener(() => OnExitButtonClick());
+
+        creditsMenuPanel.SetActive(false);
+
+
+    }
+    /// <summary>
+    /// Listener for the exit button, quits the game.
+    /// </summary>
+    void OnExitButtonClick()
+    {
+        Debug.Log("Exit Button Clicked");
+        Application.Quit();
+    }
+    /// <summary>
+    /// Listener for the start button, loads the scene where that game starts.
+    /// </summary>
+    void OnStartButtonClick()
+    {
+        Debug.Log("Start Button Clicked");
+        SceneManager.LoadScene("Game");
+    }
+    /// <summary>
+    /// Listener for credits button, opens the credits panel.
+    /// </summary>
+    void OnCreditsButtonClick()
+    {
+        Debug.Log("Credits Button Clicked");
+        mainMenuPanel.SetActive(false);
+        creditsMenuPanel.SetActive(true);
+    }
+    /// <summary>
+    /// Listener for the back button when in the credits panel, closes the credits panel and opens the main menu panel.
+    /// </summary>
+    void OnBackButtonClick()
+    {
+        Debug.Log("Back Button Clicked");
+        creditsMenuPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+}
